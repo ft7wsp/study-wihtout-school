0e552d23165076402ab4c16b13f83c422fb062e9
radhi <tliliradhi8@gmail.com>
2022-05-03 14:14:35 +0100

    cours 1er

--- src/components/YearCard.js
+++ src/components/YearCard.js
@@ -11,29 +11,31 @@ function YearCard(props) {
     const subjectsTitles = (id) => {
         switch (id) {
             case '1er':
-                return ['Anatomie',
-                    'Physiologie',
-                    'Physiologie de sang',
-                    'Génétique',
-                    'Histologie',
-                    'Cytologie',
-                    'Psychologie',
-                    'Soins dans la communauté ',
-                    ' Techniques de communication',
-                    'Idi',
-                    ' Droit de patient',
-                    '  Microbiologie',
-                    ' Informatique',
-                    ' Biochimie ',
-                    '  Éthique ',
-                    '  Démarche de soins ',
-                    '  Techniques infirmières',
-                    'Ergonomie ',
-                    ' Anglais']
+                // return ['Anatomie',
+                //     'Physiologie',
+                //     'Physiologie de sang',
+                //     'Génétique',
+                //     'Histologie',
+                //     'Cytologie',
+                //     'Psychologie',
+                //     'Soins dans la communauté ',
+                //     ' Techniques de communication',
+                //     'Idi',
+                //     ' Droit de patient',
+                //     '  Microbiologie',
+                //     ' Informatique',
+                //     ' Biochimie ',
+                //     '  Éthique ',
+                //     '  Démarche de soins ',
+                //     '  Techniques infirmières',
+                //     'Ergonomie ',
+                //     ' Anglais']
+
+                return ['diagnostic', 'physiology', 'psychology']
             case '2eme':
-                return [' Mathematiue de base 3', 'Reseaux de communication', ' Mathematiue de base 4', 'Base de donnees', 'System & Scripting', 'Fondements des Resaux', 'Arichitecture des microcontroleaurs', 'Project WEB', 'C++']
+                // return [' Mathematiue de base 3', 'Reseaux de communication', ' Mathematiue de base 4', 'Base de donnees', 'System & Scripting', 'Fondements des Resaux', 'Arichitecture des microcontroleaurs', 'Project WEB', 'C++']
             case '3eme':
-                return ['Finance', 'Environnement entreprise', 'UML', 'Java', 'IP essentials', 'Switched Net', 'Analyse numeriue', 'Calcul scientifique', 'UNIX', 'Francais', 'Genie loiciel', 'Routing', 'SGBD', 'Symfony', "Techniues de'estimatin port l'ingenieur", 'Theorie des languages et Complilation', 'Programmation des Terminaus Mobiles']
+                // return ['Finance', 'Environnement entreprise', 'UML', 'Java', 'IP essentials', 'Switched Net', 'Analyse numeriue', 'Calcul scientifique', 'UNIX', 'Francais', 'Genie loiciel', 'Routing', 'SGBD', 'Symfony', "Techniues de'estimatin port l'ingenieur", 'Theorie des languages et Complilation', 'Programmation des Terminaus Mobiles']
             default:
                 return ['no subjects found']
         }
@@ -50,7 +52,7 @@ function YearCard(props) {
                 <div className={classes.title}>
                     <div className={classes.subjects}>
                         {subjectsTitles(id).map(sub =>
-                            <a href={test} target='_blank'>  <div className={classes.subject} key={sub + Math.random()}>{sub}</div> </a>
+                            <a href={require(`./${id}/${sub}.pdf`) ? require(`./${id}/${sub}.pdf`) : require(`./Quran.pdf`)} target='_blank'>  <div className={classes.subject} key={sub + Math.random()}>{sub}</div> </a>
                         )}
                     </div>
                 </div>
